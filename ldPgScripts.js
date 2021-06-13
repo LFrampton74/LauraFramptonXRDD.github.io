75ac3d6c45b89adfbc698c88a7815757ca36f773
Laura Frampton <83966492+LFrampton74@users.noreply.github.com>
2021-06-13 18:02:54 -0400

    Add files via upload

---
+++ ldPgScripts.js
@@ -0,0 +1,17 @@
+//Week 5 - creating functions for navigation buttons
+function UATSpaceTrans(){
+    location.replace("UATSpace.html");
+}
+function indexTrans(){
+    location.replace("index.html");
+}
+function objTrans(){
+    location.replace("objectives.html");
+}
+//Additional Nav Buttons, personalized
+function xrddTrans(){
+    location.replace("https://lframpton74.wixsite.com/xrdd");
+}
+function lflinkTrans(){
+    location.replace("https://www.linkedin.com/in/laura-frampton-1454b3147/");
+}
---
+++ objectives.js
@@ -0,0 +1,14 @@
+//Week 5, functions for menu buttons to transition between pages
+function UATSpaceTrans(){
+    location.replace("UATSpace.html");
+}
+function indexTrans(){
+    location.replace("Index.html");
+}
+//Week 5, added add'l function to include previously designed page & LinkedIn Profile for GAM125
+function xrddTrans(){
+    location.replace("https://lframpton74.wixsite.com/xrdd");
+}
+function lflinkTrans(){
+    location.replace("https://www.linkedin.com/in/laura-frampton-1454b3147/");
+}
